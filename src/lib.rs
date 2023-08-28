4d5e6830b76350d8f6c543671364ad7705994935
BoManev <bomanev@pm.me>
2023-08-28 13:37:15 -0400

    basics

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,100 @@ extern "C" {
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, wasm-of-life!");
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
+}
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+#[wasm_bindgen]
+impl Universe {
+    pub fn new() -> Self {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|c| {
+                if c % 2 == 0 || c % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Self {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn get_index(&self, row: u32, col: u32) -> usize {
+        (row * self.width + col) as usize
+    }
+
+    pub fn live_neighbot_count(&self, row: u32, col: u32) -> u8 {
+        let mut count = 0;
+        // height - 1 == - 1 (row above)
+        for drow in [self.height - 1, 0, 1].iter().cloned() {
+            for dcol in [self.width - 1, 0, 1].iter().cloned() {
+                if drow == 0 && dcol == 0 {
+                    continue;
+                }
+
+                let nrow = (row + drow) % self.height;
+                let ncol = (col + dcol) % self.width;
+                let idx = self.get_index(nrow, ncol);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_count = self.live_neighbot_count(row, col);
+
+                next[idx] = match (cell, live_count) {
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    (Cell::Dead, 3) => Cell::Alive,
+                    (any, _) => any,
+                };
+            }
+        }
+        self.cells = next;
+    }
+}
+
+impl std::fmt::Display for Universe {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for line in self.cells.chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Alive { '◼' } else { '◻' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+        Ok(())
+    }
 }
